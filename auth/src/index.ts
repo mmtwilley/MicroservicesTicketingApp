3968c3e3d66a8efcde937adab851b4e20c242441
Marvin Twilley <marvinmtwilley@gmail.com>
2023-02-12 13:25:53 -0600

    The work that I completed thru far. The file contains a auth service.

---
+++ auth/src/index.ts
@@ -0,0 +1,40 @@
+import express from "express";
+import 'express-async-errors';
+import mongoose from "mongoose";
+
+import { currentUserRouter } from "./routes/current-user";
+import { signinRoute } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+import {errrorHandler} from "./middlewares/error-handler"
+import { NotFoundError } from "./errors/not-found-error";
+
+const app = express();
+app.use(express.json());
+
+
+app.use(currentUserRouter);
+app.use(signinRoute);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all('*', (req, res, next) =>{
+    next (new NotFoundError());
+});
+
+app.use(errrorHandler);
+
+const start = async() => {
+    try{
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    } catch(err){
+        console.error(err);
+    }
+    console.log('Connected to MongoDb')
+    app.listen(3000,()=>{
+        console.log('Listening on port 3000!');
+    });
+};
+
+
+start();
